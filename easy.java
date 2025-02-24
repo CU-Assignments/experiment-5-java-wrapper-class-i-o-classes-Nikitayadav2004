189e0182aaeab03190fe52bbb36aeb19a2272325
Nikitayadav2004 <nikitayadavitis@gmail.com>
2025-02-24 16:14:27 +0530

    easy.java

---
+++ easy.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoBoxingExample {
+    public static void main(String[] args) {
+        
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);  
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(parseStringToInteger("40")); 
+        numbers.add(parseStringToInteger("50"));
+
+        int sum = calculateSum(numbers);
+
+    
+        System.out.println("Sum of numbers: " + sum);
+    }
+
+  
+    public static Integer parseStringToInteger(String str) {
+        return Integer.parseInt(str); 
+    }
+
+   
+    public static int calculateSum(List<Integer> numbers) {
+        int sum = 0;
+        for (Integer num : numbers) {
+            sum += num; 
+        }
+        return sum;
+    }
+}
